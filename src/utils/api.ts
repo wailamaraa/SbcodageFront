b21210d5312096a48cc0800fc745411af3f613d9
wailamaraa <166392342+wailamaraa@users.noreply.github.com>
2025-10-06 10:49:32 +0100

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://sbcodageback.onrender.com/api';
 
 // Create an axios instance
 const api = axios.create({
